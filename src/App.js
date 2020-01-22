ea20e427a4bdbff685a18d5b84b2b66b15d89c96
Your Name <you@example.com>
2020-01-22 12:46:04 +0100

    Workińg with the auth of Teams

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import * as microsoftTeams from "@microsoft/teams-js";
+let count = 0;
+let auth = '';
 
 function App() {
+  useEffect(() => {
+  
+    if (count === 1) {
+      microsoftTeams.initialize();
+      
+      auth = microsoftTeams.authentication.authenticate({
+        url: 'http://localhost:3000',
+        width: '200px',
+        height: '100px',
+        successCallback: '',
+        failureCallback: ''
+      });
+      
+      return;
+    }
+    count++;
+  },[]);
+  console.log(auth);
+
   return (
     
     <div className="App">
