f8e44b862a04e25112647ba6450eec0633c65424
Your Name <you@example.com>
2020-01-23 11:56:51 +0100

    Workińg with the auth of Teams

--- src/App.js
+++ src/App.js
@@ -1,81 +1,77 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
 import * as microsoftTeams from "@microsoft/teams-js";
 import { UserAgentApplication } from "msal";
+import { logDOM } from '@testing-library/react';
+import Axios from 'axios';
 let count = 0;
 let auth = '';
 
-/*
-Program-ID (klient)
-:
-f419a7f3-aed6-42a9-9158-a877e14cff63
-Katalog-ID (klientorganisation)
-:
-fbb2c627-4588-4c2f-b6a4-0dfb5dc31330
-Objekt-ID
-:
-3353a44f-98f8-4730-8ca7-5adea4f3f4bf
+function App() {
+  const [ accessToken, setAccessToken ] = useState('');
+  let newInstance = null;
+
+  let loginAuth = () => {
+    // Configuration object constructed
+    let clientId = "f419a7f3-aed6-42a9-9158-a877e14cff63";
+    var msalConfig = {
+      auth: {
+        clientId: clientId,
+        authority: `https://login.microsoftonline.com/${clientId}`,
+        redirectURI: "http://localhost:3000/"
+      },
+      cache: {
+        cacheLocation: "localStorage",
+        storeAuthStateInCookie: true
+      }
+    };
+    return msalConfig;
+  }
+  let createNewInstance = () => {
+    // create UserAgentApplication instance
+    const msalObj = new UserAgentApplication(loginAuth());
+    console.log(msalObj);
+    
+    return msalObj;
+  }
+  let getAuthtoken = () => {
+    let loginRequest = {
+      scopes: ["user.read", "mail.send"], // optional Array<string>
+    };
+    createNewInstance().acquireTokenSilent(loginRequest).then(function (tokenResponse) {
+      setAccessToken(tokenResponse.accessToken);
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+    
+  useEffect(() => {
+    getAuthtoken();
+  },[]);
+  console.log(accessToken);
+
+Axios.get('https://graph.microsoft.com/v1.0/me/', {
+  headers: {
+    Bearer: accessToken
+  }
+}).then(response => {
+console.log(response);
+
+}).catch(error => {
+  console.log(error.response);
+});
+
+ /*  function authCallback(error, response) {
+    //handle redirect response
+  }
+
+  // (optional when using redirect methods) register redirect call back for Success or Error
+  msalObj.handleRedirectCallback(authCallback);
+
+
 */
 
-function App() {
-  // Configuration object constructed
-const msalConfig = {
-  auth: {
-      clientId: 'fbb2c627-4588-4c2f-b6a4-0dfb5dc31330',
-      redirectUrl: window.location
-  }
-}
 
-// create UserAgentApplication instance
-const msalObj = new UserAgentApplication(msalConfig);
-
-function authCallback(error, response) {
-  //handle redirect response
-}
-
-// (optional when using redirect methods) register redirect call back for Success or Error
-msalObj.handleRedirectCallback(authCallback);
-
-
-var loginRequest = {
-  scopes: ["user.read", "mail.send"], // optional Array<string>
-  prompt: 'select_account',
-};
-let accessTokenRequest = {
-  scopes: ["user.read", "mail.send"]
-}
-
-
-msalObj.loginPopup(loginRequest).then(function (loginResponse) {               
-  return msalObj.acquireTokenSilent(accessTokenRequest);
-}).then(function (accessTokenResponse) {
-  const token = accessTokenResponse.accessToken;
-  console.log(token);
-  
-}).catch(function (error) {  
-  //handle error
-});
-console.log(msalObj);
-
-/*   useEffect(() => {
-  
-    if (count === 1) {
-      microsoftTeams.initialize();
-      
-      auth = microsoftTeams.authentication.authenticate({
-        url: 'http://localhost:3000',
-        width: '200px',
-        height: '100px',
-        successCallback: '',
-        failureCallback: ''
-      });
-      
-      return;
-    }
-    count++;
-  },[]);
-  console.log(auth);
- */
   return (
     
     <div className="App">
