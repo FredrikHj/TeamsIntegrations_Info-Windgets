a0f343c47b76b97ccf91b05299373ed08aee8120
Your Name <you@example.com>
2020-01-24 21:42:06 +0100

    Workińg with showing data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState} from 'react';
 import './App.css';
-import * as microsoftTeams from "@microsoft/teams-js";
 import { UserAgentApplication } from "msal";
-import { logDOM } from '@testing-library/react';
 import Axios from 'axios';
 
 function App() {
   const [ accessToken, setAccessToken ] = useState('');
-
+  const [ plannerData, updatePlannerData ] = useState([]);
+  useEffect(() => {
+  },[]);
+  
   let loginAuth = () => {
     let programId = 'f419a7f3-aed6-42a9-9158-a877e14cff63';
     // Configuration object constructed
@@ -35,19 +36,14 @@ function App() {
     let loginRequest = {
       scopes: ["user.read"], // optional Array<string>
     };
-    createNewInstance().loginPopup(loginRequest).then(function (tokenResponse) {
-      setAccessToken(tokenResponse.accessToken);
-      console.log(tokenResponse);
+    createNewInstance().acquireTokenSilent(loginRequest).then(function (response) {
+      setAccessToken(response.accessToken);
+      console.log(response);
     }).catch(function (error) {
       console.log(error);
     });
   }
-    
-  useEffect(() => {
-    getAuthtoken();
-  });
   
-  console.log(accessToken);
   let runAxiosGet = (accessToken) => {
     Axios.get('https://graph.microsoft.com/v1.0/me/', {
       headers: {
@@ -55,18 +51,29 @@ function App() {
       }
     }).then(response => {
       console.log(response);
+      updatePlannerData(response.data);
       
     }).catch(error => {
       console.log(error.response);
     });
   }
+  getAuthtoken();
   runAxiosGet( accessToken );
-
+  console.log(plannerData);
+  
   return (
     
     <div className="App">
-
+      {
+          plannerData.map((data) => {
+            console.log(data);
+            return(
+                {data}
+            )
+          })
+      }
       Teams Integrations 
+
 
     </div>
   );
