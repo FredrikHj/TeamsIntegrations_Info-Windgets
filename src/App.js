91997258cbd06700efc58fab04fe4a7e09da8907
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2020-01-25 10:26:33 +0100

    Working with showing data

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ function App() {
   
   let loginAuth = () => {
     let programId = '1a29bb77-9d79-4adf-933f-c95c3d0a62e9';
+    let klientorganisationId = 'fbb2c627-4588-4c2f-b6a4-0dfb5dc31330';
     // Configuration object constructed
     var msalConfig = {
       auth: {
         clientId: programId,
-        authority: `https://login.microsoftonline.com/${programId}`,
+        authority: `https://login.microsoftonline.com/${klientorganisationId}`,
         redirectURI: "http://localhost:3000/"
       },
       cache: {
@@ -36,7 +37,7 @@ function App() {
     let loginRequest = {
       scopes: ["user.read"], // optional Array<string>
     };
-    createNewInstance().loginPopup(loginRequest).then(function (response) {
+    createNewInstance().acquireTokenSilent(loginRequest).then(function (response) {
       setAccessToken(response.accessToken);
       console.log(response);
     }).catch(function (error) {
