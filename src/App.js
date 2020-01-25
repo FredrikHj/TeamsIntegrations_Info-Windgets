f38d5db42420e11c9789a32e568445a25f5d842e
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2020-01-25 17:08:15 +0100

    Working with showing data = Done, start working with the design

--- src/App.js
+++ src/App.js
@@ -3,12 +3,53 @@ import './App.css';
 import { UserAgentApplication } from "msal";
 import Axios from 'axios';
 
+let toDoList = [
+  {
+    toDoHeadLine: 'List 1',
+    toDoCards: [
+      {
+        cardHedline: 'Card 1',
+        cardContent: 'refvrga',
+      },
+      {
+        cardHedline: 'Card 2',
+        cardContent: 'ufyyuj',
+      }
+    ]
+  },
+  {
+    toDoHeadLine: 'List 2',
+    toDoCards: [
+      {
+        cardHedline: 'Card 1',
+        cardContent: 'refvrga',
+      },
+      {
+        cardHedline: 'Card 2',
+        cardContent: 'ufyyuj',
+      }
+    ]
+  },
+  {
+    toDoHeadLine: 'List 3',
+    toDoCards: [
+      {
+        cardHedline: 'Card 1',
+        cardContent: 'refvrga',
+      },
+      {
+        cardHedline: 'Card 2',
+        cardContent: 'ufyyuj',
+      }
+    ]
+  }
+];
+
 function App() {
   const [ accessToken, setAccessToken ] = useState('');
-  const [ plannerData, updatePlannerData ] = useState([]);
-  useEffect(() => {
-  },[]);
-  
+  const [ plannerData, updatePlannerData ] = useState(toDoList);
+
+  let [ reRun, updateReRun ] = useState(0);
   let loginAuth = () => {
     let programId = '1a29bb77-9d79-4adf-933f-c95c3d0a62e9';
     let klientorganisationId = 'fbb2c627-4588-4c2f-b6a4-0dfb5dc31330';
@@ -39,6 +80,7 @@ function App() {
     };
     createNewInstance().acquireTokenSilent(loginRequest).then(function (response) {
       setAccessToken(response.accessToken);
+      updateReRun(1)
       console.log(response);
     }).catch(function (error) {
       console.log(error);
@@ -52,31 +94,37 @@ function App() {
       }
     }).then(response => {
       console.log(response);
-      updatePlannerData(response.data);
+      /* if (plannerData === null) {
+        updatePlannerData(response.data);
+      } */
       
     }).catch(error => {
       console.log(error.response);
     });
   }
+    useEffect(() => {
+      if (!plannerData) return;
+    },[]);
+    
   getAuthtoken();
   runAxiosGet( accessToken );
   console.log(plannerData);
   
-  return (
-    
+  return (    
     <div className="App">
-      Teams Integrations 
-      {(plannerData.length !== 0) 
-        ? plannerData.map((data) => {
-            console.log(data);
-            return(
-                {data}
-            )
-          })
-        : 'Datan laddades inte in!'
+      Teams Integrations
+
+      {
+
+        plannerData.map((data) => {
+          console.log(data);
+          
+
+          return (
+          <div>{ data.toDoHeadLine }</div>
+          )
+        })
       }
-
-
     </div>
   );
 }
