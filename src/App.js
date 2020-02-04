f132cd5f592105d3a9469b6ed41981ea0b4dbe21
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2020-02-04 21:46:18 +0100

    Working on the calc of max cards side

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import { runAxiosGet } from './components/data/axiosGet';
 import { toDoList } from './components/data/toDoList';
 
 function App() {
-  const [ accessToken, setAccessToken ] = useState('');
+  //const [ accessToken, setAccessToken ] = useState('');
   const [ plannerData, updatePlannerData ] = useState(toDoList);
-  let refHeightCardsObj = {}
-  let createBoxName = [];
   let refHeightCardContainer = React.createRef();
+  let domHeightObj = {};
+  let refDomHeightsObj = {};
+  let refHeightCardsArr = [];
+  let heightCardBoxesArr = [];
 
-  //React.createRef();
-
-  let getAuthtoken = () => {
+/*   let getAuthtoken = () => {
     let loginRequest = {
       scopes: ["user.read", "mail.send"] // optional Array<string>
     };
@@ -24,27 +24,59 @@ function App() {
       console.log(error);
     });
   }
-  useEffect(() => {
+ */  useEffect(() => {
     if (!plannerData) return;
   },[]);
-  console.log(accessToken);
+/*   console.log(accessToken);
   getAuthtoken();
-  runAxiosGet( accessToken );
+  runAxiosGet( accessToken ); */
   console.log(plannerData);
-  /*   let removeBlankHeadline = (toDoHeadLine) => {
-    let removeBlankHeadline = `list${toDoHeadLine.split(' ')[1]}`;
-    return removeBlankHeadline;
-  } */
+
+  /* The function willl take three argumment = calMaxCardOfContainers(arg1 ,arg2, arg3) ...
+    arg 1 = What list
+    arg 2 = Nr1 calculating
+    arg 3 = Nr2 calculating
+  */
+  let calcHeightOfCardContainers = () => {
+
+    console.log(domHeightObj);
+    let cardBoxesArr = domHeightObj.heightCardBoxesArr;
+    for (let index = 0; index < cardBoxesArr.length; index++) {
+      const element = cardBoxesArr[index];
+      console.log(element);
+      
+    }
+
+  }
   let setCardOfSide = () => {
-    console.log(refHeightCardsObj);
+    let heightCardContainer = 0;
 
-/*     setTimeout(() => {
-      refsDomObj = {
-        heightCardContainer: refCardContainer.current.offsetHeight,
-      };  
-      console.log(refsDomObj);
-    }, 1000); */
-
+    //Save it to its space
+    refDomHeightsObj['refHeightCardContainer'] = refHeightCardContainer;
+    refDomHeightsObj['refHeightCardsArr'] = refHeightCardsArr;
+    
+    setTimeout(() => {
+      let redfCardBoxesArr = refDomHeightsObj.refHeightCardsArr;
+      //refHeightCardContainer = refDomHeightsObj.refHeightCardContainer;
+      heightCardContainer = refDomHeightsObj.refHeightCardContainer.current.offsetHeight;
+      domHeightObj['cardContainer'] = heightCardContainer;
+      
+      redfCardBoxesArr.map((toDoList, countList) => {
+        
+        // Createing a array placing the boxes heights. The last index is tot in the list
+        heightCardBoxesArr.push([]);
+        domHeightObj['heightCardBoxesArr'] = heightCardBoxesArr;
+        
+        toDoList.map((toDoBoxes, countBoxes) => {
+          let presentToDoList = domHeightObj.heightCardBoxesArr[countList];
+          presentToDoList.push(refDomHeightsObj.refHeightCardsArr[countList][countBoxes].current.offsetHeight);   
+          console.log(
+            presentToDoList
+          );
+          
+        })
+      });
+    }, 1000);
   }
   
   return (
@@ -70,49 +102,40 @@ function App() {
         <section id="toDoCardContainer" ref={ refHeightCardContainer }>
           {
             plannerData.map((dataLists, listNr) => {
-
+              let createBoxName = [];
+              
               let getToDoCards = dataLists.toDoCards;
-              let listName = `list${listNr+1}`;
+
+              refHeightCardsArr.push([]);
+
 
               return(
                 <section key={ listNr } className="toDoCardsListContainer">
                   {
                     getToDoCards.map((cards, cardBoxNr) => {
-                      
-                      let cardBoxName = `toDoCardBoxes${cardBoxNr}`;
-                      console.log(cardBoxName);
-                      createBoxName.push(React.createRef());
-
-                      refHeightCardsObj[listName] = createBoxName;
+                      refHeightCardsArr[listNr].push(React.createRef());
 
                       return(
-                        <div key={ cardBoxNr } className="toDoCardBoxes" /* ref={ createBoxName[cardBoxName] } */>
+                        <div key={ cardBoxNr } className="toDoCardBoxes" ref={ refHeightCardsArr[listNr][cardBoxNr] }>
                           <div className="toDoCardHeadLine">{ cards.cardHedline }</div>
                           <hr></hr>
                           <div className="toDoCardHeadContent">{ cards.cardContent }</div>
                         </div>
                       );
+
                     })
                   }
+                  <section key={ listNr } className="toDoHeadLinesBox toDoCardSides">
+                    {`Sidan ${'1'} av ${'1'}`}
+                  </section>
                 </section>
+                
               );
             })
           }
         </section>
-        <footer id="toDoCardSidesContainer">
-          {
-            plannerData.map((dataLists, listNr) => {
-              console.log(listNr);
-              return(
-                <section key={ listNr } className="toDoHeadLinesBox toDoCardSides">
-                  {`Sidan ${'1'} av ${'1'}`}
-                </section>
-              );
-            })
-          }
-        </footer>
       </main>
-      {setCardOfSide()}
+      {setCardOfSide(calcHeightOfCardContainers())}
     </div>
   );
 }
