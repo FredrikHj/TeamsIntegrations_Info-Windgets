d047dd60af4691816e048909eb0e96014dbf072c
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2020-02-13 00:03:30 +0100

    Working with current page and showing correct data

--- src/App.js
+++ src/App.js
@@ -18,25 +18,18 @@ let count = -1;
 const App = () => {
   count++;
   //const [ accessToken, setAccessToken ] = useState('');
-  const [ toDoData ] = useState(toDoList);
-  const [ domHeightArr ] = useState([]); 
-  const [ listCardPagesArr ] = useState([]); 
-  const [ heightCardBoxesArr ] = useState([]); 
-  const [ currentCardPageArr ] = useState([]); 
-  const [ currentShowingCard ] = useState([]); 
-  const [ refHeightCardsArr ] = useState([]); 
-  const [ currentTodoData ] = useState([]); 
-  const [ getVisibleDataArr ] = useState([]); 
-  let [ loopList, updateLoopList ] = useState(0); 
+  const [ toDoData, setToDoData ] = useState(toDoList);
+  const [ refHeightCardContainer, setRefHeightCardContainer ] = useState(React.createRef()); 
+  const [ domHeightArr, setDomHeightArr ] = useState([]); 
+  const [ listCardPagesArr, setListCardPagesArr ] = useState([]); 
+
+  //const [ cardPageArr, setCardPageArr ] = useState([]); 
+  const [ showingCards, setShowingCards ] = useState([]); 
+  const [ refHeightCardsArr, setRefHeightCardsArr ] = useState([]); 
+  //const [ currentTodoData, setCurrentTodoData ] = useState([]); 
+  //const [ getVisibleDataArr, setGetVisibleDataArr] = useState([]); 
+  //let [ loopList, updateLoopList ] = useState(6);
   
-  const refHeightCardContainer = React.createRef(); 
-  let promiseFooterArr = new Promise(success => {
-    window.onload = (event) => {
-      createHeightArr();
-      success();
-    };
-    
-  })
   
   /*   let getAuthtoken = () => {
     let loginRequest = {
@@ -50,45 +43,76 @@ const App = () => {
     });
   }
   */ 
-  useEffect(() => {
-    //if (!toDoData || !refHeightCardContainerObj.current) return;
-  },[]);
-  console.log(refHeightCardContainer)
+  useEffect(() => {   
+    let promiseFooterArr = new Promise(success => {
+      window.onload = (event) => {
+        
+        success();
+      };
+      
+    })
+    promiseFooterArr.then((data) => {
+      //setFooterCalc(true);
+    })
+    saveHeightValuesIntoArr();
+    handleCardPages();
+    /*     saveHeightValuesIntoArr();
+    calcCardPages(); */
+    
+    console.log('Min toDoData :)');
+    console.log(toDoData);
+    
+    console.log('domHeightArr');
+    console.log(domHeightArr);
+  },[
+    domHeightArr, showingCards
+  ]);
+  
   /*   console.log(accessToken);
   getAuthtoken();
   runAxiosGet( accessToken ); */
   
-  let createHeightArr = () => {
-    let getNrOfCards = 0;
-    console.log(domHeightArr);
-    
-    domHeightArr.push(refHeightCardContainer.current.offsetHeight);     
-    
-    //Save it to its space
-    /*
+  let saveHeightValuesIntoArr= () => {
+    let pushToDomHeightArr = [...domHeightArr];
+    let heightCardBoxesArr = [];
+
+    /* Save the elements height according:
     Index 0 = cardContainers height.
     Index 1 = Arrays of the lists and holding the height values of the list cards 
-    Index 2 is the tot of the values for the specific list!
+    Index 2 is the tot of the values for the specific list! ???0
     */
-    domHeightArr.push(heightCardBoxesArr);
-    domHeightArr.push([]);
+    pushToDomHeightArr.push(refHeightCardContainer.current.offsetHeight);
+    console.log(pushToDomHeightArr.length !== 0);
+    pushToDomHeightArr.push(heightCardBoxesArr);
+    
+    //pushToDomHeightArr.push([]); Space for index 2 ?
     
     // Get out the offsetHeight from eatch cardelement and save the values to a common space 
-    toDoData.map((item, listIndex) => {
+    for (let listIndex = 0; listIndex < toDoData.length; listIndex++) {
+      let getIntoList = toDoData[listIndex].toDoCards;      
       heightCardBoxesArr.push([]); 
-      item.toDoCards.map((item, cardIndex) => {
-        domHeightArr[1][listIndex].push(refHeightCardsArr[listIndex][cardIndex].current.offsetHeight+cardBoxSpace /*+style.cardBoxSpace */);
-        console.log(domHeightArr[1][listIndex]);
-      })
-      calcHeighOtfCardBoxes(listIndex)
-    })
-    toDoData.map((item, listIndex) => listIndex);
+      for (const cardIndex in getIntoList) {
+        pushToDomHeightArr[1][listIndex].push(refHeightCardsArr[listIndex][cardIndex].current.offsetHeight+cardBoxSpace /*+style.cardBoxSpace */ );
+      }
+      
+    } 
+    console.log(heightCardBoxesArr);
 
+    // If the hook is emty = update it if not stop
+    if (domHeightArr.length === 0) setDomHeightArr(pushToDomHeightArr);
+    /*     toDoData.map((item, listIndex) => {
+      item.toDoCards.map((item, cardIndex) => {
+      })
+      //calcHeighOtfCardBoxes(listIndex)
+    })
+    //toDoData.map((item, listIndex) => listIndex);
+    
     refHeightCardsArr.map((item, index) => {
       console.log(refHeightCardsArr[index][1].current.offsetHeight);
       
       
-    });
+    }); */
+    
   }
   let calcHeighOtfCardBoxes = (listNr) => {
     let getIntoList = domHeightArr[1][listNr];  
@@ -96,55 +120,87 @@ const App = () => {
     
     let savedListTot = 0;
     
-    for (let index = 0; index  < getIntoList.length; index++) {
+    /* for (let index = 0; index  < getIntoList.length; index++) {
       //Save the tot of a list array at the end as the last index in that array
       savedListTot += getIntoList[index];
       if (index === getIntoList.length - 1) domHeightArr[2].push(savedListTot);
-    }
+    } */
     
     return showCardBoxPage;
+  } 
+
+  let handleCardPages = () => {
+    let cardPages = 0;
+    let cardOfPage = 0;
+
+    let pushToListCardPagesArr = [...listCardPagesArr];    
+    let heightCardContainer = domHeightArr[0];
+    
+    if (domHeightArr[1] !== undefined){
+      let heightCardBox = domHeightArr[1][0][0];
+      console.log(heightCardContainer);
+      console.log(heightCardBox);
+      
+      // This not handle dynamic cardsBoxes height just static for all cards
+      cardOfPage = Math.round(calcCardPages(heightCardContainer, heightCardBox));
+      fixListPages(pushToListCardPagesArr, cardOfPage);
+    }
   }
-  promiseFooterArr.then((data) => {
-    calcCardOfSide();
-    //setFooterCalc(true);
-  })
-  
-  let calcCardOfSide = () => {
-    let maxCardPage = 0;
-    let maxCardShowing = 0;
-    
-    let heightCardContainer = domHeightArr[0]    
-    let heightCardBox = domHeightArr[1][0][0];
-    maxCardShowing = Math.round(heightCardContainer/heightCardBox);
-    console.log(maxCardShowing);
-    
+  let calcCardPages = (nr1, nr2) => {
+    return nr1/nr2;
+  }
+  let fixListPages = (pushToListCardPagesArr, cardOfPage) => {
     for (let index = 0; index < domHeightArr[1].length; index++) {
       let cardQuantity = domHeightArr[1][index].length;
-      maxCardPage = Math.round(cardQuantity/maxCardShowing);
+      let cardPages = Math.round(cardQuantity/cardOfPage);
       
-      //Update the card / page      
-      let setListPage = maxCardPage;
-      if (setListPage === 0) setListPage = 1;
-      listCardPagesArr.push(setListPage);        
-      console.log(listCardPagesArr);
-    }    
+      //Update list pages
+      pushToListCardPagesArr.push([cardPages]);   
+      setListCardPagesArr(pushToListCardPagesArr);    
+    }
   }
-  let fixShowingCards = (index, cardData) => {   
+  console.log(domHeightArr[1]);
+  
+  let fixShowingCards = (listIndex) => {
+    let slicedList;
+    console.log(listIndex);
+    
+    let pushToShowingCards = [...showingCards ];
     let currentSideNr = 1;
     let startCardIndex = 0;
-    
     let endCardIndex = 0;
     
-    /*  startCardIndex = currentSideNr*0-0; // Start Index 
-    endCardIndex = currentSideNr*0;+// End index
-    */
+    startCardIndex = currentSideNr*0-0; // Start Index 
+    endCardIndex = currentSideNr*0;// End index
+    
     console.log(currentSideNr);
     console.log(startCardIndex);
     console.log(endCardIndex);
-    
-    //updateLoopList(loopList++);
-    return cardData/* .slice(0, 7) */;
+
+    for (let loopIndex = 0; loopIndex < toDoData.length; loopIndex++) {
+      const element = toDoData[loopIndex].toDoCards;
+      console.log(element);
+      console.log(listIndex === loopIndex);
+      
+      if (listIndex === loopIndex) {
+        let slicedList = toDoData[listIndex].toDoCards.slice(2,5);
+        console.log(slicedList);
+      }
+      
+    }
+    //let showCards = item.slice(2, 3);
+    //pushToShowingCards[index].push(item);
+
+    // Testar med olika slice
+    if (toDoData.length-1 < listIndex) {
+    pushToShowingCards.push(slicedList);
+    setShowingCards(pushToShowingCards);
+    }
+    console.log(slicedList);
+    //   return showCards;
+
   }
+  console.log(showingCards);
 
   return (
     <div id="appbody">
@@ -165,21 +221,23 @@ const App = () => {
         <section id="toDoCardContainer" ref={ refHeightCardContainer }> 
           {
             toDoData.map((item, index) => {
-              console.log(item);
+              fixShowingCards(index)
+              console.log(item.toDoCards,);
               
               // Force the push function only pushing the actual elements
               if (refHeightCardsArr.length <= index){
                 refHeightCardsArr.push([]);
-                currentCardPageArr.push([]);
-              };     
-
+                //currentCardPageArr.push([]);
+              };
+              // Show correct list and its cards
+             
               return(
                 <ToDoCards key={ index }
-                  mainTodoList={ item.toDoCards }
-                  fixShowingCards={ fixShowingCards(index, item.toDoCards) }
-                  refHeightCardsArr={ refHeightCardsArr }
-                  listIndex={ index }
-                  currentCardPageArr={ currentCardPageArr }
+                mainTodoList={ item.toDoCards }
+                fixShowingCards={ showingCards[index] }
+                refHeightCardsArr={ refHeightCardsArr }
+                listIndex={ index }
+                /* currentCardPageArr={ currentCardPageArr } */
                 />
                 );
               })
@@ -190,11 +248,11 @@ const App = () => {
       <footer id="toDoCardSidesContainer">
         {
           toDoData.map((item, index) => {
-            console.log(listCardPagesArr);
-            
+           console.log(item);
+           
             return(
               <section key={ index } className="toDoHeadLinesBox toDoCardSides">
-                {`Sid ${currentCardPageArr[index]} av ${listCardPagesArr[index]}`}
+               {`Sid ${1/* currentCardPageArr[index] */} av ${listCardPagesArr[index]}`}
               </section>
             );
           })
--- src/components/structure/toDoCards.js
+++ src/components/structure/toDoCards.js
@@ -1,24 +1,45 @@
 import React from 'react';
 export const ToDoCards = (props) => {
+console.log(props.fixShowingCards);
 
     return(
-        <section key={ props.listIndex } className="toDoitemListContainer">
-            {
-                props.mainTodoList.map((item, index) => {
-                // Force the push function only pushing the actual elements
-                
-                if (props.refHeightCardsArr[props.listIndex].length <= index) props.refHeightCardsArr[props.listIndex].push(React.createRef());
-                    console.log(props.refHeightCardsArr);
+        <>
+            <section key={ props.listIndex+1 } className="toDoitemListContainer">
+                {
+                   /*  props.fixShowingCards.map((item, index) => {
+                    // Force the push function only pushing the actual elements
                     
-                    return(
-                        <div key={ index } className="toDoCardBoxes" ref={ props.refHeightCardsArr[props.listIndex][index]}>
-                            <div className="toDoCardHeadLine">{ item.cardHedline }</div>
-                            <hr></hr>
-                            <div className="toDoCardHeadContent">{ item.cardContent }</div>
-                        </div>
-                    );
-                })
-            }
-        </section>
+                    if (props.refHeightCardsArr[props.listIndex].length <= index) props.refHeightCardsArr[props.listIndex].push(React.createRef());
+                        //console.log(props.refHeightCardsArr);
+                        
+                        return(
+                            <div key={ index } className="toDoCardBoxes" ref={ props.refHeightCardsArr[props.listIndex][index]}>
+                                <div className="toDoCardHeadLine">{ item.cardHedline }</div>
+                                <hr></hr>
+                                <div className="toDoCardHeadContent">{ item.cardContent }</div>
+                            </div>
+                        );
+                    }) */
+                }
+            </section>
+            <section key={ props.listIndex } className="toDoitemListContainer hidden">
+                {
+                    props.mainTodoList.map((item, index) => {
+                    // Force the push function only pushing the actual elements
+                    
+                    if (props.refHeightCardsArr[props.listIndex].length <= index) props.refHeightCardsArr[props.listIndex].push(React.createRef());
+                        //console.log(props.refHeightCardsArr);
+                        
+                        return(
+                            <div key={ index } className="toDoCardBoxes" ref={ props.refHeightCardsArr[props.listIndex][index]}>
+                                <div className="toDoCardHeadLine">{ item.cardHedline }</div>
+                                <hr></hr>
+                                <div className="toDoCardHeadContent">{ item.cardContent }</div>
+                            </div>
+                        );
+                    })
+                }
+            </section>
+        </>
     );
 }
