1772864b31e856fff06896787084bb444a07169e
Your Name <you@example.com>
2020-02-17 13:09:04 +0100

    backup

--- src/App.js
+++ src/App.js
@@ -77,14 +77,14 @@ const App = () => {
   }
   let calcCardInPages = () => {
     let cardOfPage = 0;
-    //let pushToListCardPagesArr = [...listCardPagesArr];    
+    let pushToListCardPagesArr = [...listCardPagesArr];    
     let heightCardContainer = domHeightArr[0];
     if (domHeightArr[1] !== undefined && toDoData.length !== 0){
       let heightCardBox = domHeightArr[1][0][0];
       
       // This not handle dynamic cardsBoxes height just static for all cards
       cardOfPage = Math.round(calcCardPages(heightCardContainer, heightCardBox));
-      //setListPagesIntoArr(pushToListCardPagesArr, cardOfPage); 
+      setListPagesIntoArr(pushToListCardPagesArr, cardOfPage); 
     }
   }
   let calcCardPages = (nr1, nr2) => {
@@ -109,6 +109,7 @@ const App = () => {
   
   let fixShowingCards = (incommingItem, listIndex) => {      
     console.log("TCL: fixShowingCards -> listIndex", listIndex)
+    console.log("TCL: fixShowingCards -> incommingItem.length-1", incommingItem.length-1)
     let slicedList = [];
     let currentSideNr = 1;
     let startCardIndex = 0;
@@ -121,14 +122,12 @@ const App = () => {
     startCardIndex = currentSideNr*cardOfPage-cardOfPage; // Start Index 
     endCardIndex = currentSideNr*cardOfPage;// End index
 
-    if (incommingItem.length > cardOfPage && listIndex <= incommingItem.length-1) slicedList = incommingItem.slice(startCardIndex, endCardIndex);
-    console.log("TCL: fixShowingCards -> incommingItem.length-1", incommingItem.length-1)
-    console.log("TCL: fixShowingCards -> incommingItem.length", incommingItem.length)
+    if (incommingItem.length > cardOfPage /* && listIndex <= listIndex */) slicedList = incommingItem.slice(startCardIndex, endCardIndex);
     console.log("TCL: fixShowingCards -> incommingItem", incommingItem)
     if (incommingItem.length < cardOfPage) slicedList = incommingItem;
     return slicedList;
   }
-
+  
   return (
     <div id="appbody">
         Teams Integrations 
@@ -136,29 +135,28 @@ const App = () => {
         <section className="toDoHeadLineContainer">
           {(toDoData.length !== 0)
             ? toDoData.map((item, index) => {        
-                return (
-                  <section key={ index } className="toDoHeadLinesBox">
+              return (
+                <section key={ index } className="toDoHeadLinesBox">
                     { item.toDoHeadLine }         
                   </section>
                 ); 
               })
-            : <NoIncommingData/> 
-          }
+              : <NoIncommingData/> 
+            }
         </section>  
         {/* Save ref to correspnding created ref in top of the component */}
         <section id="toDoCardContainer" ref={ refHeightCardContainer }> 
           {(toDoData.length !== 0) 
             ? toDoData.map((item, index) => {
               let incommingItem = item.toDoCards;
-              console.log("TCL: App -> incommingItem", incommingItem)
-              
+             
               // Force the push function only pushing the actual elements
               if (refHeightCardsArr.length <= index){
                 refHeightCardsArr.push([]);
                 //currentCardPageArr.push([]);
               };
               // Show correct list and its cards
-            
+              
               return(
                 <ToDoCards key={ index }
                 mainTodoList={ incommingItem }
